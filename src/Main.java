ff7c588c13a4906d3247f39f11a62a6617a73c9a
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-14 18:12:26 +0300

    Bug fix and refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,79 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Converter converter = new Converter();
-        String[] actions = {"+", "-", "/", "*"};
-        String[] regexActions = {"\\+", "-", "/", "\\*"};
+    public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String exp = scn.nextLine();
+        calc(exp);
+    }
+
+    public static String calc(String input) throws Exception {
+        Converter converter = new Converter();
+        String[] actions = Util.CreateActions();
+        String[] regexActions = Util.CreateRegexActions();
+
         //Определяем арифметическое действие:
-        int actionIndex=-1;
-        for (int i = 0; i < actions.length; i++) {
-            if(exp.contains(actions[i])){
-                actionIndex = i;
-                break;
-            }
-        }
+        int actionIndex = Util.ArithmeticAction(actions, input);
+
         //Если не нашли арифметического действия, завершаем программу
-        if(actionIndex==-1){
-            System.out.println("Некорректное выражение");
-            return;
+        if (actionIndex == -1){
+            throw new Exception("Некорректное выражение");
         }
+
         //Делим строчку по найденному арифметическому знаку
+        String[] data = input.split(regexActions[actionIndex]);
+
+        if (Util.CheckNumberOfOperands(data))
+            throw new Exception("Должно быть 2 операнда");
 
 
-        String[] data = exp.split(regexActions[actionIndex]);
+        if (Util.CheckNumbers(data))
+            throw new Exception("Числа должны быть больше 1 и меньше 10");
+
+        int result = 0;
+
         //Определяем, находятся ли числа в одном формате (оба римские или оба арабские)
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
             int a,b;
+
             //Определяем, римские ли это числа
             boolean isRoman = converter.isRoman(data[0]);
-            if(isRoman){
+            if (isRoman) {
                 //если римские, то конвертируем их в арабские
                 //X+V
-                //x-10
+                //x-10-
                 //v - 5
                 a = converter.romanToInt(data[0]);
                 b = converter.romanToInt(data[1]);
-
-            }else{
+            } else {
                 //если арабские, конвертируем их из строки в число
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
             }
-            //выполняем с числами арифметическое действие
-            int result;
-            switch (actions[actionIndex]){
-                case "+":
-                    result = a+b;
-                    break;
-                case "-":
-                    result = a-b;
-                    break;
-                case "*":
-                    result = a*b;
-                    break;
-                default:
-                    result = a/b;
-                    break;
-            }
 
-            if(isRoman){
+            //выполняем с числами арифметическое действие
+            result = switch (actions[actionIndex]) {
+                case "+" -> a + b;
+                case "-" -> a - b;
+                case "*" -> a * b;
+                default -> a / b;
+            };
+
+            if (isRoman) {
                 //если числа были римские, возвращаем результат в римском числе
                 System.out.println(converter.intToRoman(result));
             }
-            else{
+            else {
                 //если числа были арабские, возвращаем результат в арабском числе
                 System.out.println(result);
             }
-        }else{
+        } else {
             System.out.println("Числа должны быть в одном формате");
         }
+
+        return String.valueOf(result);
     }
 }
---
+++ src/Util.java
@@ -0,0 +1,32 @@
+package src;
+
+public class Util {
+    public static String[] CreateActions() {
+        return new String[] {"+", "-", "/", "*"};
+    }
+
+    public static String[] CreateRegexActions() {
+        return new String[] {"\\+", "-", "/", "\\*"};
+    }
+
+    public static int ArithmeticAction(String[] actions, String exp) {
+        int actionIndex = -1;
+
+        for (int i = 0; i < actions.length; i++) {
+            if (exp.contains(actions[i])){
+                actionIndex = i;
+                break;
+            }
+        }
+
+        return actionIndex;
+    }
+
+    public static boolean CheckNumberOfOperands(String[] data) {
+        return data.length != 2;
+    }
+
+    public static boolean CheckNumbers(String[] data) {
+        return Integer.parseInt(data[0]) < 1 || Integer.parseInt(data[1]) > 10;
+    }
+}
